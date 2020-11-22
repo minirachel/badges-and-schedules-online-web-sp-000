890a203566de9d8ed377f65b57790144b852d287
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-22 00:50:39 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect {|badges| badge_maker(badges)}
+end
+
+def assign_rooms(attendees)
+  room_number = []
+  attendees.index {|room| room_number << room + 1}
+  puts "Hello, #{attendee}! You'll be assigned to room #{room_number}!"
+end
+
+  # attendees.each.with_index(1) do |attendee, index|
